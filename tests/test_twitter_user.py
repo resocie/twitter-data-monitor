2d60e5310aae052edc7540c90fea21b0891c7d32
Alexandre Gomes <alegomes@gmail.com>
2018-03-02 17:00:58 -0300

    two tests runing; one was left purposely broken >:-)

--- tests/test_twitter_user.py
+++ tests/test_twitter_user.py
@@ -1,8 +1,18 @@
 import unittest
-# from twitter.twitter import Twitter
+from twitter.twitter import TwitterUser
 
 class TestTwitterUser(unittest.TestCase):
-	pass
+	def test_isonline(self):
+		user = TwitterUser('alegomes')
+
+	def test_name_retrieval(self):
+		user = TwitterUser('alegomes')
+		self.assertEqual('Alexandre Gomes', user.name)
+
+	def test_id_retrieval(self):
+		user = TwitterUser('alegomes')
+		self.assertEqual('9999', user.id)
+		
 
 if __name__ == '__main__':
     unittest.main()
---
+++ twitter/__init__.py
@@ -0,0 +1,17 @@
+import logging
+import os
+import sys
+
+
+# LEVEL = logging.DEBUG
+# LEVEL = logging.INFO
+# LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+
+# log = logging.getLogger()
+# log.setLevel(LEVEL)
+
+# output = logging.StreamHandler(sys.stdout)
+# output.setLevel(LEVEL)
+# output.setFormatter(logging.Formatter(LOG_FORMAT))
+# log.addHandler(output)
