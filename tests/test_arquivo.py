f2a7116c55eaf3fc2a78fa88814716751bcb32f9
Marcospbb <krioni.x@gmail.com>
2018-04-12 10:42:48 -0300

    funçao de criar csv modularizada e testada

--- tests/test_arquivo.py
+++ tests/test_arquivo.py
@@ -1,26 +1,34 @@
 import unittest
-from twitter.twitter import TwitterUser
+import csv
+from twitter.CsvFile import CsvBuilder
 from twitter.TwitterToolkit import TwitterAPI as Tapi
 
 class TestFileCSV(unittest.TestCase):
-	def test_file_exist(self):
-		try:
-			file = open('twitter/autores.csv',newline='')
-			file.close()
-			arquivo = True
-		except:
-			arquivo = False
-		self.assertEqual(True, arquivo)
+    def test_file_exist(self):
+        try:
+            csvfile = open('twitter/autores.csv',newline='')
+            csvfile.close()
+            arquivo = True
+        except:
+        	arquivo = False
+        self.assertEqual(True, arquivo)
 
-	def test_create_csv(self):
-		teste = Tapi.create_csv_basic()
-		self.assertEqual("", teste)
+    def test_create_csv(self):
+        CsvBuilder.create_csv_basic()
+        with open('twitter/autores.csv', 'r') as csvfile:
+            reader = csv.reader(csvfile, delimiter=';')
+            row1 = next(reader)
+            first_row = ["nome","seguidores","tweets","seguindo","curtidas"]
+            self.assertEqual(first_row,row1)
+            csvfile.close()
 
-		for user in ['Renova_BR', 'bancadaativista', 'agora_movimento']:
-			user = TwitterUser(user)
-			teste = Tapi.update_csv_new_autors(user)
+        '''
+        for user in ['Renova_BR', 'bancadaativista', 'agora_movimento']:
+            user = TwitterUser(user)
+            teste = Tapi.update_csv_new_autors(user)
 
-		self.assertEqual("certo", teste)
+        self.assertEqual("certo", teste)
+        '''
 
 if __name__ == '__main__':
     unittest.main()
--- twitter/TwitterToolkit.py
+++ twitter/TwitterToolkit.py
@@ -35,19 +35,6 @@ class TwitterAPI(tweepy.API):
 
         return tweet_list
 
-    def create_csv_basic():
-        with open('twitter/autores.csv', 'w') as csvfile:
-            writer_t = csv.writer(csvfile, delimiter=';')
-            writer_t.writerow(["nome", "seguidores", "tweets", "seguindo", "curtidas"])
-            csvfile.close()
-
-    def update_csv_new_autors(self):
-        with open('twitter/autores.csv', 'a') as csvfile:
-            writer_t = csv.writer(csvfile, delimiter=';')
-            writer_t.writerow([self.name, self.followers_count,
-            self.tweets_count, self.following_count, self.likes_count])
-            csvfile.close()
-
 
 class TweetTK:
 
